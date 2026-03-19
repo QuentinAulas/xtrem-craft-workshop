0ee9806029cef5aa6870b4083fa485215aa10d7c
nlaubal <noah.laubal@etu.u-bordeaux.fr>
2026-03-19 17:29:48 +0100

    fix merge

--- java/src/test/java/money_problem/domain/BankTest.java
+++ java/src/test/java/money_problem/domain/BankTest.java
@@ -22,8 +22,12 @@ class BankTest {
         Money result = bank.convert(new Money(EUR, 10), USD);
 
         // Then
+<<<<<<< HEAD
         assertThat(result.amount()).isEqualTo(12);
         assertThat(result.currency()).isEqualTo(USD);
+=======
+        assertThat(convertResult).isEqualTo(2);
+>>>>>>> 435243f (push avec test cassé)
     }
 
     @Test
